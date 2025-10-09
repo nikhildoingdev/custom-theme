f80bda01a5f89e5a661bbbcb24b13933c5009806
Nikhil Patel <nikhil.patel@ecomexperts.io>
2025-10-10 00:17:33 +0530

    some changes

---
+++ assets/product.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", () => {
+  document
+    .querySelector(".shopify-product-form")
+    .addEventListener("input", (evt) => {
+      console.log("input");
+      
+    });
+});
