7b5a6b0b5ce0f3804c25b40f72b3c947228f7773
Nikhil Patel <nikhil.patel@ecomexperts.io>
2025-10-10 20:35:32 +0530

    some changes

--- assets/product.js
+++ assets/product.js
@@ -2,13 +2,31 @@ document.addEventListener("DOMContentLoaded", () => {
   document
     .querySelector(".shopify-product-form")
     .addEventListener("input", (evt) => {
+      evt.preventDefault()
       const variants = JSON.parse(document.getElementById('product__variants-json').innerText);
       console.log(variants)
       const selectedVariantID = document.querySelector('.product__variant-select').value;
       const selectedVariant = variants.find(variant => variant.id == selectedVariantID);
 
       // Update price
-        document.querySelector('.product__price').innerText = formatMoney(selectedVariant.price, 'USD', 'en-US');
+      document.querySelector('.product__price').innerText = formatMoney(selectedVariant.price);
       document.querySelector('.main-image-container img').src = selectedVariant.featured_image.src;
+
+      const addToCart = document.querySelector('.product__add-to-cart');
+      if (selectedVariant.available) {
+        
+      } else {
+        addToCart.setAttribute('disabled', '');
+
+        addToCart.querySelector('span').innerText = "SOLD OUT";
+        addToCart.querySelector('span').style.borderRadius = ".25rem";
+        addToCart.querySelector('span').style.background = "#fff";
+        addToCart.querySelector('span').style.color = "#000";
+        addToCart.querySelector('span').style.cursor = "not-allowed";
+
+        document.querySelector('.product__price').style.display = 'none';
+      }
+      
+      console.log(formatMoney(100000000, 'INR', 'en-IN'))
     });
 });
--- assets/theme.js
+++ assets/theme.js
@@ -1,7 +1,8 @@
 // Helper function to format into local currency
-function formatMoney(cents, currency, locale) {
+function formatMoney(cents, currency = 'USD', locale = 'en-US') {
   return new Intl.NumberFormat(locale, {
     style: "currency",
     currency: currency,
   }).format(cents / 100);
 }
+
