eafc3b981ada6444f99dd4db094b57047724cee7
Nikhil Patel <nikhil.patel@ecomexperts.io>
2025-10-11 06:11:53 +0530

    some changes

--- assets/product.js
+++ assets/product.js
@@ -2,45 +2,70 @@ document.addEventListener("DOMContentLoaded", () => {
   document
     .querySelector(".shopify-product-form")
     .addEventListener("input", (evt) => {
-      evt.preventDefault()
-      const variants = JSON.parse(document.getElementById('product__variants-json').innerText);
-      console.log(variants)
-      const selectedVariantID = document.querySelector('.product__variant-select').value;
-      const selectedVariant = variants.find(variant => variant.id == selectedVariantID);
+      evt.preventDefault();
+      const variants = JSON.parse(
+        document.getElementById("product__variants-json").innerText
+      );
+      console.log(variants);
+      const selectedVariantID = document.querySelector(
+        ".product__variant-select"
+      ).value;
+      const selectedVariant = variants.find(
+        (variant) => variant.id == selectedVariantID
+      );
 
       // Update preview images
-      const previewImagesGrid = document.querySelector('.product-images-carousel');
+      const previewImagesGrid = document.querySelector(
+        ".product-images-carousel"
+      );
       previewImagesGrid.innerHTML = "";
-      selectedVariant.images.forEach(image => {
-        const wrapper = document.createElement('div');
-        wrapper.classList.add('preview-image-container');
-        const img = document.createElement('img');
+      selectedVariant.images.forEach((image) => {
+        const wrapper = document.createElement("div");
+        wrapper.classList.add("preview-image-container");
+        const img = document.createElement("img");
         img.src = image;
-        
-        wrapper.appendChild(img)
+
+        wrapper.appendChild(img);
         previewImagesGrid.appendChild(wrapper);
-      })
+      });
+
+      // Update main image
+      document.querySelector(".main-image-container img").src =
+        selectedVariant.images[0];
 
       // Update price
-      document.querySelector('.product__price').innerText = formatMoney(selectedVariant.price);
-      document.querySelector('.main-image-container img').src = selectedVariant.images[0];
+      document.querySelector(".product__price").innerText = formatMoney(
+        selectedVariant.price
+      );
 
-      const addToCart = document.querySelector('.product__add-to-cart');
+      // Change add to cart button status
+      const addToCart = document.querySelector(".product__add-to-cart");
       if (selectedVariant.available) {
-        
+        addToCart.removeAttribute("disabled", "");
+
+        addToCart.querySelector("span").innerText = "ADD TO CART";
+        addToCart.querySelector("span").style.borderRadius = ".25rem 0 0 .25rem";
+        addToCart.querySelector("span").style.background = "#000";
+        addToCart.querySelector("span").style.color = "#fff";
+        addToCart.querySelector("span").style.cursor = "pointer";
+        addToCart.querySelector("span").style.pointerEvents = "default";
+        document.querySelector(".product__price").style.display = "block";
       } else {
-        addToCart.setAttribute('disabled', '');
+        addToCart.setAttribute("disabled", "");
 
-        addToCart.querySelector('span').innerText = "SOLD OUT";
-        addToCart.querySelector('span').style.borderRadius = ".25rem";
-        addToCart.querySelector('span').style.background = "#fff";
-        addToCart.querySelector('span').style.color = "#000";
-        addToCart.querySelector('span').style.cursor = "not-allowed";
-
-        document.querySelector('.product__price').style.display = 'none';
+        addToCart.querySelector("span").innerText = "SOLD OUT";
+        addToCart.querySelector("span").style.borderRadius = ".25rem";
+        addToCart.querySelector("span").style.background = "#fff";
+        addToCart.querySelector("span").style.color = "#000";
+        addToCart.querySelector("span").style.cursor = "not-allowed";
+        document.querySelector(".product__price").style.display = "none";
       }
 
-      const productHandle = document.querySelector('.product__variant-select').getAttribute('data-product-handle')
-      window.history.replaceState({},'', `/products/${productHandle}?variant=${selectedVariantID}`)
+      // Update url
+      window.history.replaceState(
+        {},
+        "",
+        `/products/${selectedVariant.product_handle}?variant=${selectedVariantID}`
+      );
     });
 });
